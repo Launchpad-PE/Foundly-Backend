dc9e4ae8df651b1ae3335f8ad502b430eeed5348
Mauricio Teran Zavala <mteranzavala@gmail.com>
2026-05-30 19:03:53 -0500

    chore: Initial project setup

---
+++ src/main/java/com/foundly/foundlyplatform/FoundlyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.foundly.foundlyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundlyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoundlyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foundly/foundlyplatform/FoundlyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundly.foundlyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundlyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
