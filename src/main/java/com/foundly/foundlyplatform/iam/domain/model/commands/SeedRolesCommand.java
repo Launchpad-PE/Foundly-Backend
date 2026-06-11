91e4f9512f220e018324029dbed6f10ceb9a0456
Mauricio Teran Zavala <mteranzavala@gmail.com>
2026-06-10 23:24:19 -0500

    Revert "Merge pull request #1 from Launchpad-PE/feat/iam"
    This reverts commit 0b1497d85a98bdc615b81bf10efa0e9c0de025e7, reversing
    changes made to 81cef85a0ceaa8f205eb9d8d80e0d3e83abcb7db.

--- src/main/java/com/foundly/foundlyplatform/iam/domain/model/commands/SeedRolesCommand.java
+++
@@ -1,7 +0,0 @@
-package com.foundly.foundlyplatform.iam.domain.model.commands;
-
-/**
- * Seed roles command
- * This class represents the command to seed the roles in the system.
- */
-public record SeedRolesCommand() {}
--- src/main/java/com/foundly/foundlyplatform/shared/infrastructure/persistence/jpa/entities/AuditableAbstractPersistenceEntity.java
+++ src/main/java/com/foundly/foundlyplatform/shared/infrastructure/persistence/jpa/entities/AuditableAbstractPersistenceEntity.java
@@ -2,7 +2,6 @@ package com.foundly.foundlyplatform.shared.infrastructure.persistence.jpa.entiti
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,7 +19,6 @@ import java.util.Date;
  * instead of placing {@code @Id} and auditing fields directly.</p>
  */
 @Getter
-@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableAbstractPersistenceEntity {
